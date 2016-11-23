b824b257a3849e82c775edea8a2dd2f641f7d909
azurein <robin.cosamas@gmail.com>
2016-11-23 13:29:00 +0700

    update model

--- application/models/Update_model_3.php
+++ application/models/Update_model_3.php
@@ -8,89 +8,89 @@ class Update_model_3 extends CI_Model {
 	}
 
 	public function lastUpdate(){
-
+		
 		$query = "DELETE FROM canvas";
 		$this->db->query($query);
-		$this->db_2->get('canvas');
+		$query = $this->db_2->get('canvas');
 		foreach ($query->result() as $row) {
-		      $this->db->insert('canvas',$row);
+		   	$this->db->insert('canvas',$row);
 		}
 
 		$query = "DELETE FROM card";
 		$this->db->query($query);
-		$this->db_2->get('card');
+		$query = $this->db_2->get('card');
 		foreach ($query->result() as $row) {
-		      $this->db->insert('card',$row);
+		   	$this->db->insert('card',$row);
 		}
 
 		$query = "DELETE FROM delegate_verification";
 		$this->db->query($query);
-		$this->db_2->get('delegate_verification');
+		$query = $this->db_2->get('delegate_verification');
 		foreach ($query->result() as $row) {
-			  $this->db->insert('delegate_verification',$row);
+			$this->db->insert('delegate_verification',$row);
 		}
 
 		$query = "DELETE FROM event";
 		$this->db->query($query);
-		$this->db_2->get('event');
+		$query = $this->db_2->get('event');
 		foreach ($query->result() as $row) {
-			  $this->db->insert('event',$row);
+			$this->db->insert('event',$row);
 		}
 
 		$query = "DELETE FROM facility";
 		$this->db->query($query);
-		$this->db_2->get('facility');
+		$query = $this->db_2->get('facility');
 		foreach ($query->result() as $row) {
-			  $this->db->insert('facility',$row);
+			$this->db->insert('facility',$row);
 		}
 
 		$query = "DELETE FROM lottery";
 		$this->db->query($query);
-		$this->db_2->get('lottery');
+		$query = $this->db_2->get('lottery');
 		foreach ($query->result() as $row) {
-			  $this->db->insert('lottery',$row);
+			$this->db->insert('lottery',$row);
 		}
 
 		$query = "DELETE FROM participant";
 		$this->db->query($query);
-		$this->db_2->get('participant');
+		$query = $this->db_2->get('participant');
 		foreach ($query->result() as $row) {
 			$this->db->insert('participant',$row);
 		}
 
 		$query = "DELETE FROM participant_facility";
 		$this->db->query($query);
-		$this->db_2->get('participant_facility');
+		$query = $this->db_2->get('participant_facility');
 		foreach ($query->result() as $row) {
-			  $this->db->insert('participant_facility',$row);
+			$this->db->insert('participant_facility',$row);
 		}
 
 		$query = "DELETE FROM prize";
 		$this->db->query($query);
-		$this->db_2->get('prize');
+		$query = $this->db_2->get('prize');
 		foreach ($query->result() as $row) {
-			  $this->db->insert('prize',$row);
+			$this->db->insert('prize',$row);
 		}
 
 		$query = "DELETE FROM souvenir";
 		$this->db->query($query);
-		$this->db_2->get('souvenir');
+		$query = $this->db_2->get('souvenir');
 		foreach ($query->result() as $row) {
-			  $this->db->insert('souvenir',$row);
+			$this->db->insert('souvenir',$row);
 		}
 
 		$query = "DELETE FROM users";
 		$this->db->query($query);
-		$this->db_2->get('users');
+		$query = $this->db_2->get('users');
 		foreach ($query->result() as $row) {
-			  $this->db->insert('users',$row);
+			$this->db->insert('users',$row);
 		}
 
 		$query = "DELETE FROM verification";
 		$this->db->query($query);
-		$this->db_2->get('verification');
+		$query = $this->db_2->get('verification');
 		foreach ($query->result() as $row) {
-		      $this->db->insert('verification',$row);
+		   	$this->db->insert('verification',$row);
 		}
 
 	}
