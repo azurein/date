a4f429c1c78648e2f7e070ba1cbb636e750f7a47
azurein <robin.cosamas@gmail.com>
2016-11-25 23:43:02 +0700

    ketinggalan

--- application/models/Home_model_3.php
+++ application/models/Home_model_3.php
@@ -272,6 +272,58 @@ class Home_model_3 extends CI_Model {
 			}
 		}
 
+		$data = $this->print_card_data($new_participant_id);
+		return $data;
+	}
+
+	public function print_card_data($participant_id)
+	{
+		$query = 	"SELECT DISTINCT
+					event.event_name,
+					event.event_img,
+					event.address,
+					b.facility_name as table_name,
+					participant.follower,
+					titles.title_name,
+					participant.participant_name,
+					groups.group_name,
+					RIGHT(card.card_id, 3) AS lottery_num
+
+					FROM event
+
+					JOIN participant
+					ON event.event_id = participant.event_id
+					AND event._status <> 'D'
+					AND participant._status <> 'D'
+
+					JOIN titles
+					ON participant.title_id = titles.title_id
+					AND titles._status <> 'D'
+
+					JOIN card
+					ON participant.participant_id = card.participant_id
+					AND card._status <> 'D'
+
+					JOIN groups
+					ON participant.group_id = groups.group_id
+
+					LEFT JOIN participant_facility
+					ON participant.participant_id = participant_facility.participant_id
+					AND participant_facility._status <> 'D'
+
+					LEFT JOIN facility a
+					ON participant_facility.facility_id = a.facility_id
+					AND a._status <> 'D'
+
+					LEFT JOIN facility b
+					ON a.facility_parent_id = b.facility_id
+					AND b._status <> 'D'
+
+					WHERE event.is_active = '1'
+					AND event.event_id = $_SESSION[event_id]
+					AND participant.participant_id = $participant_id";
+
+		$data = $this->db->query($query)->result_array();
 		return $data;
 	}
 }
